b7f93336b441c65a61ae88ec17512a99f99c1fb8
Leonardo Bocos <LE23080289@merida.tecnm.mx>
2024-09-05 11:07:06 -0600

    AppBar added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:p1_donut_app_leonardo_bocos/pages/home_pages.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,6 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return const MaterialApp(home: HomePage());
   }
 }
