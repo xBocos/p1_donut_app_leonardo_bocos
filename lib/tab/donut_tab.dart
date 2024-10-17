1f7edc8f482e9d36fcb2d31ff033dc384f45c84e
Leonardo Bocos <LE23080289@merida.tecnm.mx>
2024-10-17 10:54:59 -0600

    DonutTile added

--- lib/tab/donut_tab.dart
+++ lib/tab/donut_tab.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:p1_donut_app_leonardo_bocos/utils/donut_tile.dart';
 
 class DonutTab extends StatelessWidget {
+  // list of donuts
+  final List donutsOnSale = const [
+// [ donutFlavor, donutPrice, donutColor, imageName ]
+    ["Ice Cream", "36", Colors.blue, "lib/images/icecream_donut.png"],
+    ["Strawberry", "45", Colors.red, "lib/images/strawberry_donut.png"],
+    ["Grape Ape", "84", Colors.purple, "lib/images/grape_donut.png"],
+    ["Choco", "95", Colors.brown, "lib/images/chocolate_donut.png"],
+  ];
   const DonutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("DonutTab"));
+    //Delegate sirve para organizar los widgets en la cuadricula
+    return GridView.builder(
+        itemCount: donutsOnSale.length,
+        padding: const EdgeInsets.all(12),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, childAspectRatio: 1 / 1.5),
+        itemBuilder: (context, index) {
+          return DonutTile(
+            donutFlavor: donutsOnSale[index][0],
+            donutPrice: donutsOnSale[index][1],
+            donutColor: donutsOnSale[index][2],
+            imageName: donutsOnSale[index][3],
+          );
+        });
   }
 }
---
+++ lib/utils/donut_tile.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class DonutTile extends StatelessWidget {
+  final String donutFlavor;
+  final String donutPrice;
+  final dynamic donutColor; //dynamic porque sera de tipo color
+  final String imageName;
+
+  const DonutTile(
+      {super.key,
+      required this.donutFlavor,
+      required this.donutPrice,
+      this.donutColor,
+      required this.imageName});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(color: donutColor[50]),
+    );
+  }
+}
